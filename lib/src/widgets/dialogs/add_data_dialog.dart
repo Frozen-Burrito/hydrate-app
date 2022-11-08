617397f574853eae9eed1c446a1da3b5eef087bb
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-11-08 09:22:12 -0600

    activar condicionalmente las opciones de registro de datos

--- lib/src/widgets/dialogs/add_data_dialog.dart
+++ lib/src/widgets/dialogs/add_data_dialog.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:hydrate_app/src/models/user_profile.dart';
 import 'package:hydrate_app/src/routes/route_names.dart';
+import 'package:hydrate_app/src/services/profile_service.dart';
+import 'package:provider/provider.dart';
 
 class AddDataDialog extends StatelessWidget {
 
   const AddDataDialog({Key? key}) : super(key: key);
+
+  VoidCallback? _getOptionOnPressed(BuildContext context, _AddDataNavOption option, UserProfile? profile) {
+
+    if (profile == null) return null;
+
+    switch (option.route) {
+      case RouteNames.newActivity:
+      case RouteNames.weeklyForm:
+        return () => Navigator.popAndPushNamed(context, option.route);
+      case RouteNames.medicalForm:
+        if (profile.hasRenalInsufficiency || profile.hasNephroticSyndrome) {
+          return () => Navigator.popAndPushNamed(context, option.route);
+        } else {
+          return null;
+        }
+      default: 
+        return null;
+    }
+  }
 
   static const List<_AddDataNavOption> options = <_AddDataNavOption>[
     _AddDataNavOption( 
@@ -25,47 +47,59 @@ class AddDataDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text("Agrega datos"),
-      content: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.4,
-        width: double.maxFinite,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text("Registra datos sobre tu actividad, estilo de vida, o salud:"),
+    return Consumer<ProfileService>(
+      builder: (context, profileService, __) {
+        return FutureBuilder<UserProfile?>(
+          future: profileService.profile,
+          builder: (context, snapshot) {
 
-            const SizedBox(height: 8.0,),
+            final profile = snapshot.data;
 
-            ListView.builder(
-              shrinkWrap: true,
-              itemBuilder: (context, i) {
-          
-                final option = options[i];
-          
-                return ListTile(
-                  minVerticalPadding: 8.0,
-                  leading: option.icon,
-                  title: Text(option.title),
-                  contentPadding: const EdgeInsets.all( 16.0, ),
-                  trailing: const Icon(
-                    Icons.arrow_forward,
-                    size: 24.0,
-                  ),
-                  onTap: () => Navigator.popAndPushNamed(context, option.route),
-                );
-              },
-              itemCount: options.length,
-            ),
-          ],
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () => Navigator.pop(context, null), 
-          child: const Text('Cancelar'),
-        ),
-      ],
+            return AlertDialog(
+              title: const Text("Agrega datos"),
+              content: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.4,
+                width: double.maxFinite,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text("Registra datos sobre tu actividad, estilo de vida, o salud:"),
+
+                    const SizedBox(height: 8.0,),
+
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemBuilder: (context, i) {
+                  
+                        final option = options[i];
+                  
+                        return ListTile(
+                          minVerticalPadding: 8.0,
+                          leading: option.icon,
+                          title: Text(option.title),
+                          contentPadding: const EdgeInsets.all( 16.0, ),
+                          trailing: const Icon(
+                            Icons.arrow_forward,
+                            size: 24.0,
+                          ),
+                          onTap: _getOptionOnPressed(context, option, profile),
+                        );
+                      },
+                      itemCount: options.length,
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, null), 
+                  child: const Text('Cancelar'),
+                ),
+              ],
+            );
+          }
+        );
+      }
     );
   }
 }
